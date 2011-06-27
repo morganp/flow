cac11b4100665b4c7e0fe338c6a7b70385205062
Morgan Prior <morgan@amaras-tech.co.uk>
2011-06-27 20:44:08 +0100

    Adding A better Active record query for users with articles

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,8 +1,7 @@
 class AboutController < ApplicationController
   
   def index
-    @users = User.find(:all, :order => "name desc")
-    @users.keep_if{ |user| user.articles.size > 0 }
+    @users = User.includes(:articles).where(["articles.id IS NOT ?", nil]).order("name")
   end
   
 end
